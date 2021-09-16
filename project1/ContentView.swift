c3fd7365cc0914a9e300df65c672754e110524e4
Zakee Khattak <zakhattak@csu.fullerton.edu>
2021-09-16 15:13:34 -0700

    Initial Commit

---
+++ project1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  project1
+//
+//  Created by Zakee Khattak on 9/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
