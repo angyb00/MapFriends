3bee33492c3ca8833643757fba58d79daf4a69a5
Joji Thomas <33244451+jthomas2234@users.noreply.github.com>
2021-09-21 21:52:06 -0700

    Added skeleton code to work on login auth and splash screen

---
+++ project1Tests/loginTest.swift
@@ -0,0 +1,37 @@
+//
+//  loginTest.swift
+//  project1Tests
+//
+//  Created by Joji on 9/21/21.
+//
+
+import XCTest
+
+//Will eventually delete this boiler plate code and replace with
+//test that will check for password length and to see if the user
+//has already been registered in the system.
+
+
+class loginTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
