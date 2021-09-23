d3fab2526f280ec810aeb41ab2a22a140d46ef02
angyb00 <89486108+angyb00@users.noreply.github.com>
2021-09-23 13:46:04 -0700

    Created settings structure.

---
+++ project1/settings.swift
@@ -0,0 +1,32 @@
+import Foundation
+import SwiftUI 
+
+
+struct settings{
+ 
+  
+  let email:String
+  let firstName:String
+  let lastName: String
+  
+  
+  func edit_profile(){
+    
+  }
+  
+  func darkMode(){
+
+    
+  }
+  
+  func logOut(){
+
+  }
+  
+  
+  func securityPreference(){
+    
+    
+  }
+  
+}
