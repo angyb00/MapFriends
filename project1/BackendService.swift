2609d0e581bb56c657e88f05de3e29c47b317abb
Zakee Khattak <zakhattak@csu.fullerton.edu>
2021-09-23 12:40:31 -0700

    Added BackendService with placeholder stub functions for backend functionality.

---
+++ project1/BackendService.swift
@@ -0,0 +1,48 @@
+//
+//  BackendService.swift
+//  project1
+//
+//  Created by Zakee Khattak on 9/23/21.
+//
+
+import Foundation
+
+func backendLogin(username: String, password: String) {
+    
+}
+
+func backendLogout() {
+    
+}
+
+func backendGetLocations() -> [String] {
+    return []
+}
+
+func backendGetSublocations(at location: String) -> [String] {
+    return []
+}
+
+func backendGetGroups(at location: String) -> [String] {
+    return []
+}
+
+func backendJoinGroup(_ group: String) {
+    
+}
+
+func backendLeaveGroup(_ group: String) {
+    
+}
+
+func backendCreateGroup(name: String, at location: String) {
+    
+}
+
+func backendGetReviews(for sublocation: String, at location: String) -> [String] {
+    return []
+}
+
+func backendPostReview(for sublocation: String, at location: String, text: String) {
+    
+}
