c83e9d432a900871af276f3d043bf65ccef6d0dd
angyb00 <89486108+angyb00@users.noreply.github.com>
2021-10-07 12:19:41 -0700

    Update settings.swift

--- project1/settings.swift
+++ project1/settings.swift
@@ -5,28 +5,5 @@ import SwiftUI
 struct settings{
  
   
-  let email:String
-  let firstName:String
-  let lastName: String
-  
-  
-  func edit_profile(){
-    
-  }
-  
-  func darkMode(){
-
-    
-  }
-  
-  func logOut(){
-
-  }
-  
-  
-  func securityPreference(){
-    
-    
-  }
   
 }
