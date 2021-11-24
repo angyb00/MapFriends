cd8077863123a3d0bcc708dd0fdebffbf15d4747
Joji Thomas <33244451+jthomas2234@users.noreply.github.com>
2021-11-23 20:23:22 -0800

    Add files via upload
    Load which groups still exist even if app has been closed

---
+++ ExistingGroups.swift
@@ -0,0 +1,45 @@
+//
+//  ExistingGroups.swift
+//  project1
+//
+//  Created by Joji Thomas on 11/20/2021.
+//
+
+import SwiftUI
+
+struct ExistingGroups: View {
+    @State var listItems = Create.shared.existingGroups
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View {
+        List {
+            ForEach(listItems, id: \.self) { (item) in
+                getText(fromGrp: item)
+            }.onDelete(perform: self.deleteItem)
+        }
+        Button("Dismiss") {
+            self.presentationMode.wrappedValue.dismiss()
+        }
+    }
+
+    private func deleteItem(at indexSet: IndexSet) {
+        Create.shared.delete(at: indexSet)
+        self.listItems = Create.shared.existingGroups
+    }
+
+    private func getText(fromGrp item:Create.Group) -> some View {
+        let grp = """
+Name: \(item.groupName)
+Location: \(item.location)
+Point Of Interest: \(item.poi)
+"""
+        return Text(grp)
+
+    }
+    
+}
+
+struct ExistingGroups_Previews: PreviewProvider {
+    static var previews: some View {
+        ExistingGroups()
+    }
+}
