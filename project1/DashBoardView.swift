60699c67903d156ef94740322438914ce21ffad7
angyb00 <89486108+angyb00@users.noreply.github.com>
2021-12-02 23:55:31 -0800

    Update DashBoardView.swift

--- project1/DashBoardView.swift
+++ project1/DashBoardView.swift
@@ -1,51 +1,44 @@
+
+
 import MapKit
 import SwiftUI
-import UIKit
 
-struct MapView: UIViewRepresentable {
-    var coordinate: CLLocationCoordinate2D
+struct DashBoardView: View {
+    @State var showCreateGroup = false
+    @State var showGroups = false
 
-    func makeUIView(context: Context) -> MKMapView {
-        MKMapView(frame: .zero)
-    }
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            TabView {
+                // Map view with groups
+                firstTab()
+                    .tabItem {
+                        Label("Map", systemImage: "1.circle")
+                    }
+                    .tag(1)
 
-    func updateUIView(_ view: MKMapView, context: Context) {
-        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
-        let region = MKCoordinateRegion(center: coordinate, span: span)
-        view.setRegion(region, animated: true)
+                // Friends list tab
+                friendsList()
+                    .tabItem {
+                        Label("Friends", systemImage: "2.circle")
+                    }
+                    .tag(2)
+
+                // Settings view
+                LoginPage()
+                    .tabItem {
+                        Label("Settings", systemImage: "3.circle")
+                    }.tag(3)
+
+            }.onAppear {
+                loadGroups()
+            }
+        }
     }
 }
 
-struct DashBoardView: View {
-    @State var latitude: CLLocationDegrees = 34.052235
-    @State var longitude: CLLocationDegrees = -118.243683
-
-    var body: some View {
-        // Tabs to transition to different views
-        TabView {
-            MapView(coordinate: CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
-                .padding(.top, 30)
-                .font(.callout)
-                .tabItem {
-                    Label("Map", systemImage: "1.circle")
-                }
-                .tag(1)
-            //friends list view
-            friendsList()
-                // .padding()
-                .font(.callout)
-                .tabItem {
-                    Label("Friends", systemImage: "2.circle")
-                }
-                .tag(2)
-            //Settings view
-            settingsView()
-                .font(.callout)
-                .tabItem {
-                    Label("Settings", systemImage: "3.circle")
-                }.tag(3)
-        }
-    }
+func loadGroups() {
+    DataManager.shared.loadAllGroups()
 }
 
 struct DashBoardView_Previews: PreviewProvider {
