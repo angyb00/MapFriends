64b8a062d7e0df1bc29bb361b4a127b041b57258
angyb00 <89486108+angyb00@users.noreply.github.com>
2021-12-03 12:16:25 -0800

    Update settingsView.swift

--- project1/settingsView.swift
+++ project1/settingsView.swift
@@ -1,8 +1,26 @@
 import SwiftUI
 
+// View will decide whether logged in or log out is pressed
+struct settings: View {
+    @State var loggedOut: Bool = false
+
+    var body: some View {
+        // If log out is pressed, go back to login view
+        if loggedOut {
+            LoginAuth()
+        }
+        // Stay on settings view if logged in
+        else {
+            settingsView(loggedOut: $loggedOut)
+        }
+    }
+}
+
 struct settingsView: View {
-    @State var username = ""
+    @State var username = "John Doe"
     @State var notiEnabled = false
+    @Binding var loggedOut: Bool // Dictates what view settings will show
+
     var body: some View {
         NavigationView {
             Form {
@@ -21,6 +39,14 @@ struct settingsView: View {
                         Text("1.0")
                     }
                 }
+                Section {
+                    // Button for logging out
+                    Button(action: { loggedOut = true }, label: { Text("Log out")
+                            .bold()
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .foregroundColor(.red)
+                    })
+                }
             }.navigationTitle("Settings")
         }
     }
@@ -28,7 +54,6 @@ struct settingsView: View {
 
 struct settingsView_Previews: PreviewProvider {
     static var previews: some View {
-        settingsView()
+        settings()
     }
 }
-
