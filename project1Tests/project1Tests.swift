7f6f24deed13d2a4c1fd3f95aaaf5434d62f007f
angyb00 <89486108+angyb00@users.noreply.github.com>
2021-12-07 12:51:34 -0800

    Update project1Tests.swift

--- project1Tests/project1Tests.swift
+++ project1Tests/project1Tests.swift
@@ -1,11 +1,5 @@
-//
-//  project1Tests.swift
-//  project1Tests
-//
-//  Created by Zakee Khattak on 9/16/21.
-//
-
 import XCTest
+import SwiftUI
 @testable import project1
 
 class project1Tests: XCTestCase {
@@ -42,20 +36,81 @@ class project1Tests: XCTestCase {
     }
     
     func testSettingsView(){
-        let settings = settingsView()
-        XCTAssertFalse(settings.notiEnabled)
+        
+       
+        let settings = settingsView(loggedOut: .constant(false))
+        XCTAssertFalse(settings.loggedOut)
         XCTAssertEqual(settings.username, "John Doe")
         
 
         
     }
     func testMainView(){
-        let MainView = DashBoardView()
-        
-        XCTAssertEqual(MainView.longitude, -118.243683)
-        XCTAssertEqual(MainView.latitude, 34.052235)
-        
+        let MainView = firstTab()
+
+        XCTAssertFalse(MainView.showGroups)
+        XCTAssertFalse(MainView.reviewMode)
+        XCTAssertFalse(MainView.showCreateGroup)
+
+    }
+    
+
+}
+import XCTest
+import SwiftUI
+@testable import project1
+
+class project1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testFriendsList(){
+        let friendsList = friendsList()
+        
+        
+        XCTAssertEqual(friendsList.listOfPeople.count, 5)
+        XCTAssertFalse(friendsList.addFriend)
+        XCTAssertEqual(friendsList.friendName, "")
+        
+       
+    }
+    
+    func testSettingsView(){
+        
+       
+        let settings = settingsView(loggedOut: .constant(false))
+        XCTAssertFalse(settings.loggedOut)
+        XCTAssertEqual(settings.username, "John Doe")
+        
+
+        
+    }
+    func testMainView(){
+        let MainView = firstTab()
+
+        XCTAssertFalse(MainView.showGroups)
+        XCTAssertFalse(MainView.reviewMode)
+        XCTAssertFalse(MainView.showCreateGroup)
+
+    }
+    
 
 }
