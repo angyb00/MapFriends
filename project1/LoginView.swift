ca9cd1540a6e7ac8b00e527597451bd8df3a3619
angyb00 <89486108+angyb00@users.noreply.github.com>
2021-12-07 15:36:23 -0800

    Update LoginView.swift

--- project1/LoginView.swift
+++ project1/LoginView.swift
@@ -1,48 +1,40 @@
 import SwiftUI
 
-
 struct LoginView: View {
-    
     @State var username: String = ""
     @State var password: String = ""
     
-    @Binding var signedIn:Bool  //Dictates whether logged in or not
+    @Binding var signedIn: Bool // Dictates whether logged in or not
     
     fileprivate func WelcomeMessage() -> Text {
         return Text("Travel Together!")
-            
     }
     
     var body: some View {
         VStack {
-            VStack{
-                WelcomeMessage()
-                    .bold()
-                    .font(.largeTitle)
-                Image("plane")
-                    .resizable()
-                    .font(.largeTitle)
+            WelcomeMessage()
+                .bold()
+                .font(.largeTitle)
+            Image("plane")
+                .resizable()
+                .font(.largeTitle)
                     
-                TextField("Username", text: $username)
-                    .padding()
-                    .cornerRadius(5.0)
-                    .padding(.bottom,20)
+            TextField("Username", text: $username)
+                .padding()
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
                     
-                
-                SecureField("Password",text :$password)
-                    .padding()
-                    .cornerRadius(5.0)
-                    .padding(.bottom,20)
+            SecureField("Password", text: $password)
+                .padding()
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
                     
-
-                Button(action: {self.signedIn = true},label: {Text("Login")})
-                
-
-            }
-        }
-        .padding()
+            Button(action: { self.signedIn = true
+                BackendService.login(username: username, password: password)
+                    
+            }, label: { Text("Login") })
+        }.padding()
     }
-    
 }
 
 struct LoginView_Previews: PreviewProvider {
