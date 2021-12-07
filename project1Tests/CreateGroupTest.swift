cbab3c6b9b4144249cde7570b5e6c578776c1313
angyb00 <89486108+angyb00@users.noreply.github.com>
2021-12-07 15:28:53 -0800

    Update CreateGroupTest.swift

--- project1Tests/CreateGroupTest.swift
+++ project1Tests/CreateGroupTest.swift
@@ -1,89 +1,70 @@
-//
-//  CreateGroup.swift
-//  project1Tests
-//
-//  Created by Joji Thomas 10/20/2021.
-//
-
-import XCTest
 @testable import project1
+import XCTest
 
 class CreateGroupTest: XCTestCase {
     private var sampleGroupName = "Group1"
     private var sampleLocation = "Paris"
     private var samplePointOfInterest = "Eiffel Tower"
-    private var sampleGroup:Create.Group!
-    private var createObj:Create!
+    private var sampleGroup: Create.Group!
+    private var createObj: Create!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-        self.createObj = Create.shared
-        self.sampleGroup = Create.Group(groupName: self.sampleGroupName, location: self.sampleLocation, poi: self.samplePointOfInterest,groupNumber: self.createObj.getNextGroupNumber())
-
+        createObj = Create.shared
+        sampleGroup = Create.Group(groupName: sampleGroupName, location: sampleLocation, poi: samplePointOfInterest, groupNumber: createObj.getNextGroupNumber())
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        self.createObj = nil
-        self.sampleGroup = nil
+        createObj = nil
+        sampleGroup = nil
     }
 
-    func test_onCreateSharedObjectExistingGroupsIsEmpty(){
-        XCTAssertEqual(createObj.existingGroups.count,0,"shared object on creation should not have any existing groups")
-    }
+    func test_validSampleGroupName() {
+        // given sameple group creation
 
-    func test_validSampleGroupName(){
-        //given sameple group creation
-
-        //then
+        // then
         XCTAssertEqual(sampleGroup.groupName, sampleGroupName, "Expected \(sampleGroupName), Found \(sampleGroup.groupName)")
     }
 
-    func test_invalidSampleGroupName(){
-        //given invalid sampleGroup name
+    func test_invalidSampleGroupName() {
+        // given invalid sampleGroup name
         let invalidGroupName = "Group12"
 
-        //then
+        // then
         XCTAssertNotEqual(sampleGroup.groupName.lowercased(),
                           invalidGroupName.lowercased(),
                           "sampleGroup name was found to errorneously matching invalidGroup name")
     }
 
-    func test_validSampleLocation(){
-        //given sameple group creation
+    func test_validSampleLocation() {
+        // given sameple group creation
 
-        //then
+        // then
         XCTAssertEqual(sampleGroup.location, sampleLocation, "Expected \(sampleLocation), Found \(sampleGroup.location)")
     }
 
-    func test_inValidSampleLocation(){
-        //given invalid sampleGroup name
+    func test_inValidSampleLocation() {
+        // given invalid sampleGroup name
         let invalidLocationName = "Tokyo"
 
-        //then
+        // then
         XCTAssertNotEqual(sampleGroup.location.lowercased(),
                           invalidLocationName.lowercased(),
                           "Invalid location name was found to be matched against valid location of sample Group")
     }
 
-    func test_validSamplePointOfInterest(){
-        //given sameple group creation
+    func test_validSamplePointOfInterest() {
+        // given sameple group creation
 
-        //then
+        // then
         XCTAssertEqual(sampleGroup.poi, samplePointOfInterest, "Expected \(samplePointOfInterest), Found \(sampleGroup.poi)")
     }
 
-    func test_sampleGroupNameDoesNotExist(){
-        //given sampleGroup does not exist in groups
-        //checkExistence should return false
-        XCTAssertEqual(createObj.checkExistance(name: sampleGroupName),false,"expected sample group not to be present, was found to be present")
-    }
-
-
-    func test_checkGroupExistenceAfterAddingGroup(){
-        XCTAssertFalse(createObj.existingGroups.contains(where: {$0.groupName == sampleGroup.groupName}))
+    func test_checkGroupExistenceAfterAddingGroup() {
+        XCTAssertFalse(createObj.existingGroups.contains(where: { $0.groupName == sampleGroup.groupName }))
         createObj.addGroup(group: sampleGroup)
-        XCTAssertTrue(createObj.existingGroups.contains(where: {$0.groupName == sampleGroup.groupName}))
+        XCTAssertTrue(createObj.existingGroups.contains(where: { $0.groupName == sampleGroup.groupName }))
         XCTAssertTrue(createObj.existingGroups.count == 1)
     }
 }
