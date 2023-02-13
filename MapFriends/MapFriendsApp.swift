065f463227a2034dc37e4237d0a6a8d2a0ea1f0f
Angarag Gansukh <angaraggansukh@csu.fullerton.edu>
2023-02-12 19:51:48 -0800

    Refactor.

--- MapFriends/MapFriendsApp.swift
+++ MapFriends/MapFriendsApp.swift
@@ -1,19 +1,12 @@
 import SwiftUI
 import FirebaseCore
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-                FirebaseApp.configure()
-
-    return true
-  }
-}
-
 @main
 struct MapFriendsApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    
+        
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             let loginVM = AuthViewModel()
